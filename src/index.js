fb6253d0102b842f50ee0a1990b457dfa5e5a2b0
Briana <analikesu@gmail.com>
2023-04-03 17:46:50 -0400

    added updateCity function that replaces time zone (example/example) with formatted name/location and date .
    depending on what user chooses, updates html to city name, date, time and am/pm.

--- src/index.js
+++ src/index.js
@@ -24,3 +24,22 @@ function displayTime() {
 }
 displayTime();
 setInterval(displayTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#allcities");
+  citiesElement.innerHTML = ` <div class="cities">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "hh:mm:ss"
+          )}<small>${cityTime.format(" A")}</small></div>
+        </div>
+  `;
+}
+let citiesSelect = document.querySelector("#cityselect");
+citiesSelect.addEventListener("change", updateCity);
